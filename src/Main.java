03c9a806e9af7857bee51a9267fa80058ea959a5
Smirnow Grigory <smirnowgkgu44@gmail.com>
2023-03-19 18:59:18 +0300

    dictionary

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-
+            //какое-то изменение
 
             Scanner scan = new Scanner(System.in);
             boolean exit = false;
