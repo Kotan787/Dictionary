5a43d9e83276a5e8391ecf176fe8d9df21193d78
Grigory <smirnowgkgu44@gmail.com>
2023-03-23 17:08:29 +0300

    озарение

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.Scanner;
+/*
+ 2 словаря с раными регулярками от интерфейса
+ выбор словаря
+ переиминовать нормально переменные
+ обрабатывать оишбки нормально
+ */
 public class Main {
     public static void main(String[] args) {
 
