bf84ece4eb22729d345a686c5e588e8c50f4e442
Grigory <smirnowgkgu44@gmail.com>
2023-03-27 12:54:58 +0300

    чучуть ближе к концу

--- src/IMyDictionary.java
+++ src/IMyDictionary.java
@@ -1,7 +1,6 @@
 public interface IMyDictionary//интерфейс для класса словаря
 {
  //   public void swapValueToKey();
-
     public void readFromFile(String path);
     public void dictionaryPutString(String string);
     public void deleteKey(String key);
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /*
  2 словаря с раными регулярками от интерфейса
- выбор словаря
+ +выбор словаря
  переиминовать нормально переменные
  обрабатывать оишбки нормально
  */
@@ -31,10 +31,12 @@ public class Main {
                 switch (menuinput) {
                     case 1:
                         //   dictionaryActive = dictionaryFirst;
+                        System.out.println("Словарь 1 выбран \n");
                         exit = false;
                         break;
                     case 2:
                         //      dictionaryActive = dictionarySecond;
+                        System.out.println("Словарь 2 выбран \n");
                         exit = false;
                         break;
                     case 3:
@@ -50,7 +52,7 @@ public class Main {
                                     + "3. Удаление записи по ключу в словаре" + "\n"
                                     + "4. Поиск записи по ключу в словаре" + "\n"
                                     + "5. Добавление записи по ключу удовлетворяющей условиям в словарь" + "\n"
-                                    + "6. Перевернуть словарь" + "\n"
+                                    + "6. Сменить словарь" + "\n"
                                     + "7. Выход" + "\n");
                     menuinput = scan.nextInt();
 
@@ -90,19 +92,19 @@ public class Main {
 
                             break;
                         case 6:
-//                       dictionaryFirst.swapValueToKey();
-//                       System.out.println("Словарь перевернут");
+                            exit = true;
+                            choseDictionary = false;
                             break;
                         case 7:
                             exit = true;
-                            choseDictionary = false;
+                            choseDictionary = true;
                             break;
                     }
                 }
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+          System.out.println(e.getMessage());
         }
     }
 }
